ef9c6095ff661e52293a45a5cee0651a02e300ac
drajawat <dv269119@dal.ca>
2023-02-02 20:52:11 -0400

    react base

---
+++ tutorial2/src/App.js
@@ -0,0 +1,19 @@
+import logo from './jewelry.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+		<h1> Sparkle! </h1>
+        
+      </header>
+	  <body>
+	  <img src={logo} alt="logo" />
+	  <p> Shop your Jewelry here ... </p>
+	  </body>
+	  
+    </div>
+  );
+}
+export default App;
